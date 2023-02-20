e8e289eaba5031100d02d4c27c34107fc63e88b3
Felipe Basso <fe.wesleybasso@gmail.com>
2023-02-20 20:11:36 -0300

    Equals validator - Função simples de validação

---
+++ src/functions/string/equals.validator.ts
@@ -0,0 +1,7 @@
+const stringEqualsValidator = (value: string, compareValue: string): boolean => {
+
+    return value == compareValue;
+
+};
+
+export { stringEqualsValidator };
--- src/mappings/string/string-functions.mapping.ts
+++ src/mappings/string/string-functions.mapping.ts
@@ -1,10 +1,12 @@
+import { stringEqualsValidator } from "../../functions/string/equals.validator";
 import { stringMaxLengthValidator } from "../../functions/string/max-length.validator";
 import { stringMinLengthValidator } from "../../functions/string/min-length.validator";
 
 const stringFunctionsMapping = {
 
     minLengthValidatorFn: stringMinLengthValidator,
-    maxLengthValidatorFn: stringMaxLengthValidator
+    maxLengthValidatorFn: stringMaxLengthValidator,
+    equalsValidatorFn: stringEqualsValidator
 
 };
 
