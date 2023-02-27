e9377bf0c973a34377b8906094afd271b02a0e59
Felipe Basso <fe.wesleybasso@gmail.com>
2023-02-26 23:33:53 -0300

    Atualização - main.ts - testes

--- src/main.ts
+++ src/main.ts
@@ -9,8 +9,8 @@ interface IUser {
 
 const model: IUser = {
 
-    name: 'Felipe ',
-    age: 16
+    name: 'Felipe',
+    age: 23
 
 };
 
@@ -19,30 +19,9 @@ class UserValidator extends ModelValidation<IUser> {
     constructor() {
         
         super();
-
-        // this.addStringValidator('name')
-        //     .must(name => name.length > 0)
-        //         .setMessage("Nome é obrigatório")
-        //         .nextValidation()
-        //     .equals("Felipe")
-        //         .setMessage("Nome incorreto")
-        //         .nextValidation()
-        //     .notEquals("Wesley")
-        //         .setMessage("Nome incorreto (\"Wesley\" não permitido)")
-        //         .completeValidation();
-
-        this.stringValidator('name', { typeErrorMessage: 'O nome deve ser um texto' })
-            .equals("FelipE", { applyTrim: true, ignoreCase: true })
-                .setMessage("Nome incorreto")
-                .completeValidation();
     }
 }
 
 const userValidator = new UserValidator();
-
-// userValidator.addStringValidator("name")
-//     .must(name => name.lastIndexOf('e') != name.length - 1)
-//         .setMessage("Último caracter inválido")
-//         .completeValidation();
 
 userValidator.run(model);
